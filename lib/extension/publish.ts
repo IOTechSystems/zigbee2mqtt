07ba61e0e17e8f86570ca49a9ae9873837036653
AIM289 <aim289@outlook.com>
2022-09-13 14:53:55 +0100

    XRT-1819: Added transaction to error metadata

--- lib/extension/publish.ts
+++ lib/extension/publish.ts
@@ -287,7 +287,7 @@ export default class Publish extends Extension {
                         `Publish '${parsedTopic.type}' '${key}' to '${re.name}' failed: '${error}'`;
                     logger.error(message);
                     logger.debug(error.stack);
-                    this.legacyLog({type: `zigbee_publish_error`, message, meta: {friendly_name: re.name}});
+                    this.legacyLog({type: `zigbee_publish_error`, message, meta: {friendly_name: re.name, transaction: transactionID}});
                 }
 
                 usedConverters[endpointOrGroupID].push(converter);
