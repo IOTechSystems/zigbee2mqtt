88d8d752aa6f9161cc8a0eb2973c0dc72ef60d29
AIM289 <aim289@outlook.com>
2022-11-18 12:21:43 +0000

    XRT-1956: added dot

--- scripts/build.sh
+++ scripts/build.sh
@@ -3,7 +3,7 @@
 
 ARCHIVE=$1
 
-docker build --no-cache --tag $EXP/zigbee2mqtt:$VER-$ARCH --build-arg BUILD_VER=$VER --build-arg BUILD_SHA=$SHA --file ./docker/Dockerfile
+docker build --no-cache --tag $EXP/zigbee2mqtt:$VER-$ARCH --build-arg BUILD_VER=$VER --build-arg BUILD_SHA=$SHA --file ./docker/Dockerfile .
 
 if [ "$ARCHIVE" = true ]; then
 
